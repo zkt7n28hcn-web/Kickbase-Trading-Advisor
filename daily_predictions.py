937c3a41c4c5ef3a053caa79c63740faf36580e3
zkt7n28hcn-web <zkt7n28hcn@privaterelay.appleid.com>
2026-09-07 22:02:16 +0200

    Update daily_predictions.py

--- daily_predictions.py
+++ daily_predictions.py
@@ -58,7 +58,7 @@ competition_ids = [1]                   # 1 = Bundesliga, 2 = 2. Bundesliga, 3 =
 league_name = "Kempten WG"  # Name of your league, must be exact match, can be done via env or hardcoded
 start_budget = 51_000_000               # Starting budget of your league, used to calculate current budgets of other managers
 league_start_date = "2025-09-07"        # Start date of your league, used to filter activities, format: YYYY-MM-DD
-email = os.getenv("malte.krohn@krohns.de")         # Email to send recommendations to, can be the same as EMAIL_USER or different
+email = os.getenv("Email_USER")         # Email to send recommendations to, can be the same as EMAIL_USER or different
 # ---------------------------------------------------
 
 # Load environment variables and login to kickbase
