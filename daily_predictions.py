3618a067cbdbb6ea5c2808d757a82ba8144f6438
zkt7n28hcn-web <zkt7n28hcn@privaterelay.appleid.com>
2026-09-07 22:01:08 +0200

    Update daily_predictions.py

--- daily_predictions.py
+++ daily_predictions.py
@@ -55,11 +55,10 @@ pd.set_option("display.width", 1000)
 # Adjust these settings to your preferences
 
 competition_ids = [1]                   # 1 = Bundesliga, 2 = 2. Bundesliga, 3 = La Liga
-league_name = "Die 10 Nuggatschleusen"  # Name of your league, must be exact match, can be done via env or hardcoded
-start_budget = 50_000_000               # Starting budget of your league, used to calculate current budgets of other managers
-league_start_date = "2025-08-08"        # Start date of your league, used to filter activities, format: YYYY-MM-DD
-email = os.getenv("EMAIL_USER")         # Email to send recommendations to, can be the same as EMAIL_USER or different
-
+league_name = "Kempten WG"  # Name of your league, must be exact match, can be done via env or hardcoded
+start_budget = 51_000_000               # Starting budget of your league, used to calculate current budgets of other managers
+league_start_date = "2025-09-07"        # Start date of your league, used to filter activities, format: YYYY-MM-DD
+email = os.getenv("malte.krohn@krohns.de")         # Email to send recommendations to, can be the same as EMAIL_USER or different
 # ---------------------------------------------------
 
 # Load environment variables and login to kickbase
